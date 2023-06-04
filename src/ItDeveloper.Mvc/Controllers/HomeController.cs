f5bf324e61bdaf0bb52f63b1e7ee2e728a08c66d
andretorelli <andreltorelli@gmail.com>
2023-06-04 19:38:09 -0300

    Add project files.

---
+++ src/ItDeveloper.Mvc/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using ItDeveloper.Mvc.Models;
+
+namespace ItDeveloper.Mvc.Controllers;
+
+[Route("")]
+[Route("gestao-de-pacientes")]
+[Route("api/[controller]")]
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    // public IActionResult Index(Guid id, string paciente)
+    [Route("")]
+    [Route("pagina-inicial")]
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [Route("sobre/{id}/{paciente}/{categoria}")]
+    [Route("quem-somos")]
+    [Route("sobre-nos")]
+    public IActionResult Sobre(Guid id, string paciente, string categoria)
+    {
+        return View();
+    }
+
+    // public IActionResult Dashboard()
+    // {
+    //     return View();
+    // }
+
+
+    [Route("politica-de-privacidade")]
+    [Route("privacidade")]
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [Route("icones")]
+    public IActionResult Icones()
+    {
+        return View();
+    }
+
+    [Route("erro")]
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ src/ItDeveloper.Mvc/Controllers/ProdutoController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ItDeveloper.Mvc.Controllers
+{
+    [Route("[controller]")]
+    public class ProdutoController : Controller
+    {
+        private readonly ILogger<ProdutoController> _logger;
+
+        public ProdutoController(ILogger<ProdutoController> logger)
+        {
+            _logger = logger;
+        }
+
+        [Route("produto")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Lista()
+        {
+            return View();
+        }
+
+
+    }
+}
